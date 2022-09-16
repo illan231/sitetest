4b07479545bdfcc4e90c5ff52260b0b2e4f62a97
illan231 <85356303+illan231@users.noreply.github.com>
2022-09-16 18:42:15 +0200

    Delete connextion directory

--- connextion/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-    try
-    {
-        $bdd = new PDO('mysql:host=localhost;dbname=hallo bient etre;charset=utf8', 'root', '');
-    }catch(Exception $e)
-    {
-        die('Erreur' .$e->getMessage());
-    }
